d77a96867ff929e7a9df21e19e0b63a14851690b
David <goodostalm@gmail.com>
2026-08-21 11:42:38 +0300

    adding dynamism to components

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -4,13 +4,23 @@ import TodoInfo from "./TodoInfo"
 import TodoList from "./TodoList"
 
 const Todo = () => {
+    const tasks = [
+        {id: 'task-1', title: "Win ufc champion belt", isDone: false},
+        {id: 'task-2', title: "Sleep", isDone: true},
+    ]
+
     return (
         <div className="todo">
             <h1 className="todo__title">To Do List</h1>
             <AddTaskForm/>
             <SearchTaskForm/>
-            <TodoInfo/>
-            <TodoList/>
+            <TodoInfo
+                total = {tasks.length}
+                done = {tasks.filter(({isDone}) => isDone).length}
+            />
+            <TodoList
+                tasks={tasks}
+            />
         </div>
     )
 }
