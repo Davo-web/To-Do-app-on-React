8f379aec6a43f4b995a3603c2cd8f917fe956a26
David <goodostalm@gmail.com>
2026-08-22 11:22:40 +0300

    Event handlers have been added

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -9,17 +9,44 @@ const Todo = () => {
         {id: 'task-2', title: "Sleep", isDone: true},
     ]
 
+    const deleteAllTasks = () => {
+        console.log('delete all tasks')
+    }
+
+    const deleteTask = (taskId) => {
+        console.log(`delete task with id: ${taskId}`)
+    }
+
+    const toggleTaskComplete = (taskId, isDone) => {
+        console.log(`task with id: ${taskId} ${isDone ? 'done' : 'undone'}`)
+    }
+
+    const filterTasks = (query) => {
+        console.log(`searching: ${query}`)
+    }
+
+    const addTask = () => {
+        console.log('task added')
+    }
+
     return (
         <div className="todo">
             <h1 className="todo__title">To Do List</h1>
-            <AddTaskForm/>
-            <SearchTaskForm/>
+            <AddTaskForm
+                addTask = {addTask}
+            />
+            <SearchTaskForm
+                onSearchInput = {filterTasks}
+            />
             <TodoInfo
                 total = {tasks.length}
                 done = {tasks.filter(({isDone}) => isDone).length}
+                onDeleteAllButtonClick = {deleteAllTasks}
             />
             <TodoList
                 tasks={tasks}
+                onDeleteTaskButtonClick = {deleteTask}
+                onTaskCompleteChange = {toggleTaskComplete}
             />
         </div>
     )
