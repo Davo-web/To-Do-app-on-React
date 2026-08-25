e46ee726e3e0c99fe65b4f9ce484ad9171ff64c5
David <goodostalm@gmail.com>
2026-08-25 21:40:34 +0300

    Added saving to localStorage using useEffect

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,22 +3,30 @@ import TodoItem from "./TodoItem"
 const TodoList = (props) => {
     const {
         tasks =[],
+        filteredTasks,
         onDeleteTaskButtonClick,
         onTaskCompleteChange,
     } = props
 
 
-    const hasTasks = true;
+    const hasTasks = tasks.length > 0;
+    const isEmptyFilteredTasks = filteredTasks?.length === 0
     
     if (!hasTasks) {
         return (
-            <div className="todo__empty-message"></div>
+            <div className="todo__empty-message">There are not tasks yet</div>
+        )
+    }
+
+    if (isEmptyFilteredTasks && hasTasks) {
+        return (
+            <div className="todo__empty-message">Tasks not found</div>
         )
     }
 
     return (
         <ul className="todo__list">
-            {tasks.map(task => (
+            {(filteredTasks ?? tasks).map(task => (
                 <TodoItem
                     {...task}
                     className = 'todo__item'
